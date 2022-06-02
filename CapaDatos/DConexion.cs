46257a50484b5109c59bb1443b4f904771069c0e
esilva <everth-rls1@hotmail.com>
2022-06-02 12:33:16 -0600

    Agregar archivos de proyecto.

---
+++ CapaDatos/DConexion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class DConexion
+    {
+        public static String CnBDEmpresa = "Data Source=IT;Initial Catalog=BDEmpresa;User ID=sa;Password=Programacion20";
+        public static String CnMaster = "Data Source=IT;Initial Catalog=BDEmpresa;User ID=sa;Password=Programacion20";
+
+        public String ChequearConexion()
+        {
+            String mensaje = "";
+            SqlConnection SqlConexion = new SqlConnection();
+
+            try
+            {
+                SqlConexion.ConnectionString = DConexion.CnBDEmpresa;
+                SqlConexion.Open();
+                mensaje = "Y";
+            }
+            catch (Exception ex)
+            {
+                mensaje = ex.Message;
+            }
+            finally
+            {
+                SqlConexion.Close();
+            }
+
+            return mensaje;
+        }
+    }
+
+}
